a90b212cab49c2bfea7932ca780ee53e190e461b
xxy-mm <xxy.moming@gmail.com>
2024-01-27 17:46:41 +0800

    refactor: flat component file structure

---
+++ apps/dappazon-react/src/components/Product.tsx
@@ -0,0 +1,20 @@
+import { Item } from "@xxy/dappazon-contract/src/types";
+import { ethers } from "ethers";
+import { FC, PropsWithChildren } from "react";
+import { Rating } from "./Rating";
+type ProductProps = PropsWithChildren<{ product: Item }>;
+export const Product: FC<ProductProps> = ({ product }) => {
+  return (
+    <div className="flex flex-col gap-2 overflow-hidden text-ellipsis text-sm mt-4  hover:scale-105 transition-transform duration-1000">
+      <img
+        src={product.image}
+        className="cursor-pointer"
+      />
+      <span className="">{product.name}</span>
+      <div className="max-w-20 flex">
+        <Rating value={Number(product.rating)} />
+      </div>
+      <span>{ethers.formatEther(product.price)} ETH</span>
+    </div>
+  );
+};
--- apps/dappazon-react/src/components/ProductList.tsx
+++ apps/dappazon-react/src/components/ProductList.tsx
@@ -1,13 +1,13 @@
 import { Item } from "@xxy/dappazon-contract/src/types";
 import { FC } from "react";
-import Product from "../Product/Product";
+import { Product } from "./Product";
 
 type ProductsListProps = {
   products: Item[];
 };
 export const ProductList: FC<ProductsListProps> = ({ products }) => {
   return (
-    <ul className="product-list">
+    <ul className="w-full flex justify-center gap-1 md:gap-4 sm:gap-2">
       {products.map((product) => (
         <li key={product.id}>
           <Product product={product} />
